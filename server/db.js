85ab8beea878513b983cd61384e724a5f148a47a
MohammadWS <mohammadwalomair@gmail.com>
2024-11-27 04:31:53 -0500

    db host name pass

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:DNe83HWbm3PeE9fKYbYBYvQV@172.21.133.6:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
