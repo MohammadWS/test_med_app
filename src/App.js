f68e297b0bb8e9c26f3f0e81794f474344383906
MohammadWS <mohammadwalomair@gmail.com>
2024-11-28 02:32:18 -0500

    FindDoctorSearch added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/Landing_Page.js';
 import SignUp from './Components/Sign_Up/Sign_Up.js';
 import Login from './Components/Login/Login.js';
 import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation.js';  
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+// Correct imports for free FontAwesome icons
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+// Add the icons to the library
+library.add(fas);
+
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
                 <Navbar />
                 <Routes>
-                <Route path="/" element={<Landing_Page/>}/>
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/signup" element={<SignUp/>}/>
-                <Route path="/instant-consultation" element={<InstantConsultation />} />
+                    <Route path="/" element={<Landing_Page />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route path="/instant-consultation" element={<InstantConsultation />} />
                 </Routes>
             </BrowserRouter>
         </div>
     );
 }
 
-// Export the App component as the default export
 export default App;
