34582ac7f40b641f2ad3b7b595624bd0d7fd585c
MohammadWS <mohammadwalomair@gmail.com>
2024-12-03 04:00:44 -0500

    style changes

--- src/Components/DoctorCard/DoctorCard.js
+++ src/Components/DoctorCard/DoctorCard.js
@@ -111,9 +111,9 @@ const DoctorCard = ({ name, speciality, experience, ratings, profilePic }) => {
                     onClose={() => setShowModal(false)}
                 >
                     {(close) => (
-                        <div className="doctorbg" style={{ height: '100vh', overflow: 'scroll' }}>
+                        <div className="doctorbg" style={{ height: '100vh' }}>
                             <div>
-                                <div className="doctor-card-profile-image-container">
+                                <div className="doctor-card-profile-image-container justify-center flex">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" /> </svg>
                                 </div>
                                 <div className="doctor-card-details">
@@ -128,7 +128,7 @@ const DoctorCard = ({ name, speciality, experience, ratings, profilePic }) => {
                                 <>
                                     <h3 style={{ textAlign: 'center' }}>Appointment Booked!</h3>
                                     {filteredAppointment.map((appointment) => (
-                                        <div className="bookedInfo" key={appointment.id}>
+                                        <div className="m-4 p-4" key={appointment.id}>
                                             <p><span className="font-bold">Name:</span>   {appointment.name}</p>
                                             <p><span className="font-bold">Phone Number:</span>   {appointment.phoneNumber}</p>
                                             <button className="book-btn cancel-btn" onClick={() => handleCancel(appointment.id)}>Cancel Appointment</button>
